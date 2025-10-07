48dab6971771f3c3a5002dddcba95fc5331ad00a
anushree0605 <anuharini0605@gmail.com>
2025-10-07 04:01:56 -0700

    \new,\getAll endpoint hairstyle completed

---
+++ hairstyle.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request, jsonify
+app=Flask(__name__)
+from models import HairStyle
+
+@app.post('/new')
+def newHairStyle():
+    try:
+        data=request.get_json()
+        image=data.get("image")
+        title=data.get("title")
+        description=data.get("description")
+        category=data.get("category")
+        if not image or not title or not description or not category:
+            return jsonify({"status:error","message:required all  the messages"})
+        
+        HairStyle(
+            image=image,
+            title=title,
+            description=description,
+            category=category,
+
+        ).save()
+        return jsonify({"status:success","message:User added successfully"})
+    except Exception as e:
+        return({"status":"error","message":f"Error{str(e)}"})
+    
+@app.get("/getAll")
+def getAllHairStyle():
+    try:
+        hairStyles=HairStyle.object()
+        hairStylesList=[]
+
+        for hairstyle  in hairStyles:
+            data={
+                "id":hairstyle.id,
+                "image":hairstyle.image,
+                "title":hairstyle.title,
+                "description":hairstyle.description,
+                "category":hairstyle.category,
+                "isSaved":hairstyle.isSaved,
+                "shareLink":hairstyle.shareLink,
+
+            }
+
+            hairStylesList.append(data)
+
+            return jsonify({"status":"success","message":"hairStyles retrived successfully","data": hairStylesList})
+        
+    except Exception as e:
+        return({"status":"error","message":f"Error{str(e)}"}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+ 
