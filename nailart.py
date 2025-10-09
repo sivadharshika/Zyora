a0970853aa3a18afc6a49440337ce08d6cf98169
inbaarasi <iinbaarasip@gmai.com>
2025-10-09 16:27:57 +0530

    /updated endpoint Nailart completed

--- nailart.py
+++ nailart.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template,request,jsonify
-app=Flask(__name__)
 from models import NailArt
+from app import app
+from datetime import datetime
+
 
 @app.post('/new')
-def newNailArt():
+def NewNailArt():
     try:
         data=request.get_json()
+
         category =data.get("category")
         title=data.get("title")
         image=data.get("image")
         isSaved=data.get("isSaved")
         availableOn=data.get("availableOn")
-
-        if not category or not title or not availableOn or not image:
+        if not category or not title or not availableOn or not image or not isSaved:
             return jsonify({"status": "error", "message" : "All feild are requred"})
+        
         NailArt(
             category=category,
             title=title,
@@ -27,10 +30,10 @@ def newNailArt():
     
     
 @app.get("/getAll")
-def getAllnailart ():
+def getAllNailart ():
     try:
         nailArts = NailArt.objects()
-        NailArtlist =[]
+        nailArtlist =[]
 
         for nailart in nailArts:
             data={
@@ -42,14 +45,38 @@ def getAllnailart ():
                 "availableOn":nailart.availableOns,
             }
 
-            NailArtlist.append(data)
+            nailArtlist.append(data)
 
-            return jsonify({"status":"success", "message":"NailArt retrived successfully.","data": NailArtlist })
+            return jsonify({"status":"success", "message":"NailArt retrived successfully.","data": nailArtlist })
     except Exception as e:
         return({"status":"error", "message": f"Error{str(e)}"})
-
-
-
-
-
     
+    
+@app.post('/update')
+def updateNailArt():
+    try:
+        id=request.args.get("id")
+        data=request.get_json()
+        category =data.get("category")
+        title=data.get("title")
+        image=data.get("image")
+        isSaved=data.get("isSaved")
+        availableOn=data.get("availableOn")
+        if not category or not title or not availableOn or not image:
+            return jsonify({"status": "error", "message" : "All feild are requred"})
+        
+        nailArt=NailArt.object(id=id).first()
+        if not nailArt:
+            return jsonify({"status": "error", "message" : "nailart not found "})
+    
+        nailArt.category=category,
+        nailArt.title=title, 
+        nailArt.image=image,
+        nailArt.isSaved=isSaved,
+        nailArt.availableOn=availableOn,
+        
+        nailArt.updatetime=datetime.now()
+        return jsonify ({"status:success","message:Nailart added successgully"})
+    
+    except Exception as e:
+        return({"status":"error","message":f"Error{ str(e)}"})
