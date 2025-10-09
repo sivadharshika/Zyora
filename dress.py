08090e82892fe88f4c9003fbd343dcd87d0c0799
sivadharshika <siva0076374@gmail.com>
2025-10-09 16:21:21 +0530

    Update Endpoint Dress Completed

--- dress.py
+++ dress.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from models import Dress
 from app import app
+from datetime import datetime
 
 
 @app.get('/')
@@ -58,6 +59,41 @@ def getAllDress():
         return jsonify({"status": "success", "message": "Dresses retrived successfully.", "data": dressesList})
     except Exception as e:
         return({"status": "error", "message": f"Error{str(e)}"})
+
+@app.post('/update')
+def UpdateDress():
+
+    try:
+        id=request.args.get("id")
+        data=request.get_json()
+
+        image = data.get("image")
+        title = data.get("title")
+        description = data.get("description")
+        category = data.get("category")
+        availableOn = data.get("availableOn")
+
+        if not image or not title or not description or not category or not availableOn:
+            return({"status":"error", "message":"Required all the messages"})
+
+
+        dresses = Dress.object(id=id).first()
+        if not Dress:
+            return({"status":"error", "message":"Dresses not found"})
+        dresses.image = image,
+        dresses.title = title,
+        dresses.descriptio = description,
+        dresses.category = category,
+        dresses. availableOn = availableOn,
+
+        dresses.updatetime = datetime.now()
+
+        
+
+        return jsonify({"status": "success", "message": "Dress Updated successfully."})
+    except Exception as e:
+        return({"status": "error", "message": f"Error{str(e)}"})
+    
     
 
 
