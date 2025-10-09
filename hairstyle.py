8d2af7f43573bc4eef8a4a40348f9cbcfa21d952
anushree0605 <anuharini0605@gmail.com>
2025-10-09 03:54:27 -0700

    /updated endpoint hairstyle completed

--- hairstyle.py
+++ hairstyle.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 app=Flask(__name__)
 from models import HairStyle
+from datetime import datetime
 
 @app.post('/new')
 def newHairStyle():
@@ -48,30 +49,34 @@ def getAllHairStyle():
         
     except Exception as e:
         return({"status":"error","message":f"Error{str(e)}"}) 
+    
 
 
 
-
-
-
-
-
-
-
-
-
-
+@app.post('/update')
+def updateHairStyle():
+    try:
+        id=request.args.get("id")
+        data=request.get_json()
+        image=data.get("image")
+        title=data.get("title")
+        description=data.get("description")
+        category=data.get("category")
+        if not image or not title or not description or not category:
+            return jsonify({"status" :"error","message" :"required all  the messages"})
         
-
         
+        hairStyles=HairStyle.object(id=id).first()
+        if not hairStyles:
+            return jsonify({"status":"success","message":"hairstyle not found"})
+        
+        hairStyles.image=image,
+        hairStyles.title=title,
+        hairStyles.description=description,
+        hairStyles.category=category,
+        hairStyles.updatedTime=datetime.now()
+        return jsonify({"status":"success","message":"hairstyle updated successfully"})
 
-
-
-
-
-
-
-
-
-
- 
+    except Exception as e:
+        return({"status":"error","message":f"Error{str(e)}"})
+    
